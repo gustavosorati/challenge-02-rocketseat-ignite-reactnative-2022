12119862594e0bba6bc40520a5d37ec1d034e964
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-31 02:35:10 -0200

    chore: add @expo-google-fonts/nunito

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
 
+import { useFonts } from 'expo-font'
+import { Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito'
+
+import { Text } from 'react-native'
+
 import theme from 'src/styles/theme'
 
 export default function App () {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
 
+      { fontsLoaded ? <Text>Carregou</Text> : <Text>Não carregou</Text> }
     </ThemeProvider>
   )
 }
